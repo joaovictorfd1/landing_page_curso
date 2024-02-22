b47397e9576fd6afc3eec9d1343602972a8c8ef0
joaovictorfd1 <joaovictorffd1@gmail.com>
2024-02-21 22:31:57 -0300

    feat: finalizado desktop, falta agora só fazer o corte na imagem e diminui-la

--- src/components/Main/main.tsx
+++ src/components/Main/main.tsx
@@ -1,6 +1,17 @@
 import React from "react"
 import * as Yup from 'yup';
-import { Box, Button, Container, FormControl, Grid, InputLabel, MenuItem, Select, TextField, Typography } from "@mui/material"
+import {
+  Box,
+  Button,
+  Container,
+  FormControl,
+  Grid,
+  InputLabel,
+  MenuItem,
+  Select,
+  TextField,
+  Typography
+} from "@mui/material"
 import imagemProgramacao from '../../assets/img/programacao.jpeg'
 import AccessTimeIcon from '@mui/icons-material/AccessTime';
 import Card from '@mui/material/Card';
@@ -112,140 +123,144 @@ const Page = () => {
       <Box component={'div'} display={'flex'} width={'100%'} minHeight={'200px'}>
         <img src={imagemProgramacao} alt="imagem_programador" width={'100%'} height={'auto'} />
       </Box>
-      <Container maxWidth='xl'>
-        <Box component={'div'} display={'flex'} justifyContent={'center'} mt={'8px'}>
-          <Typography variant="h4">
-            Sobre o curso
-          </Typography>
-        </Box>
-      </Container>
-      <Container maxWidth='xl'>
-        {cardsInformations.map((item, index) => (
-          <Card sx={{ minWidth: '245px', margin: '16px 0px', backgroundColor: '#F7F9FF' }} key={index}>
-            <CardContent>
-              <Box component={'div'} display={'flex'} flexDirection={'column'} alignItems={'center'} gap={'8px'}>
-                <Box component={'div'} display={'flex'}>{item.icone}</Box>
-                <Box component={'span'} display={'flex'} flexWrap={'nowrap'} fontWeight={'bold'} textTransform={'uppercase'}>{item.title}</Box>
-                <Box component={'span'} display={'flex'} flexWrap={'nowrap'}>{item.informations}</Box>
-              </Box>
-            </CardContent>
-          </Card>
-        ))}
-      </Container>
-      <Container maxWidth='xl'>
-        <Box component={'div'} display={'flex'} alignItems={'center'} flexDirection={'column'} border={'1px solid #CCCCCC'} borderRadius={4}>
-          <Typography variant="inherit" lineHeight={1.5} padding={'16px 16px'}>
-            Você ama desafios? Sonha em criar tecnologia que muda o mundo? Então dê o primeiro passo para uma carreira emocionante com o Curso de Engenharia da Computação!
-            Por que escolher Engenharia da Computação conosco?
-          </Typography>
-          {AccordionInformations.map((item, index) => (
-            <Accordion key={index + 1} sx={{ width: '100%' }}>
-              <AccordionSummary
-                expandIcon={<ExpandMoreIcon />}
-                aria-controls={`panel${index + 1}-content`}
-                id={`panel${index + 1}-header`}
-              >
-                {item.title}
-              </AccordionSummary>
-              <AccordionDetails>
-                {item.informations}
-              </AccordionDetails>
-            </Accordion>
-          ))}
-        </Box>
-      </Container>
-      <Box component={'div'} m={'16px 0px'}>
-        <Container maxWidth='xl'>
-          <Typography variant="h4" display={'flex'} justifyContent={'center'}>Formulário de Inscrição</Typography>
-          <Box component={'div'} sx={{ mt: 3 }}>
-            <Grid container spacing={2}>
-              <Grid item xs={12} sm={12} md={12}>
-                <TextField
-                  autoComplete="given-name"
-                  name="nome"
-                  fullWidth
-                  id="nome"
-                  label="Nome *"
-                  autoFocus
-                  value={formik.values.nome}
-                  onChange={formik.handleChange}
-                  error={Boolean(formik.errors.nome)}
-                />
-              </Grid>
-              <Grid item xs={12} sm={12} md={12}>
-                <TextField
-                  fullWidth
-                  id="sobrenome"
-                  label="Sobrenome *"
-                  name="sobrenome"
-                  autoComplete="family-name"
-                  value={formik.values.sobrenome}
-                  onChange={formik.handleChange}
-                  error={Boolean(formik.errors.sobrenome)}
-                />
-              </Grid>
-              <Grid item xs={12} sm={12} md={12}>
-                <TextField
-                  type="email"
-                  fullWidth
-                  id="email"
-                  label="Email *"
-                  name="email"
-                  autoComplete="email"
-                  value={formik.values.email}
-                  onChange={formik.handleChange}
-                  error={Boolean(formik.errors.email)}
-                />
-              </Grid>
-              <Grid item xs={12} sm={12} md={12}>
-                <TextField
-                  fullWidth
-                  name="telefone_celular"
-                  label="Telefone Celular *"
-                  id="telefone_celular"
-                  autoComplete="cellphone"
-                  value={formik.values.telefone_celular}
-                  onChange={formik.handleChange}
-                  InputProps={{
-                    inputComponent: ReactInputMask as any,
-                    inputProps: {
-                      mask: "(99)99999-9999",
-                      maskChar: " ",
-                    },
-                  }}
-                  error={Boolean(formik.errors.telefone_celular)}
-                />
-              </Grid>
-              <Grid item xs={12} sm={12} md={12}>
-                <FormControl fullWidth>
-                  <InputLabel id="-label">Horário *</InputLabel>
-                  <Select
-                    fullWidth
-                    labelId="-label"
-                    id="horario"
-                    name="horario"
-                    label="Horário *"
-                    value={formik.values.horario}
-                    onChange={formik.handleChange}
-                    error={Boolean(formik.errors.horario)}
-                  >
-                    <MenuItem value={'14as16'}>Das 14h às 18h</MenuItem>
-                    <MenuItem value={'19as23'}>Das 19h às 23h</MenuItem>
-                  </Select>
-                </FormControl>
-              </Grid>
+      <Box component={'div'} display={'flex'}>
+        <Grid container spacing={2}>
+          <Grid item md={8} sm={12}>
+            <Box component={'div'} display={'flex'} justifyContent={'center'} mt={'8px'}>
+              <Typography variant="h4">
+                Sobre o curso
+              </Typography>
+            </Box>
+            <Grid container>
+              {cardsInformations.map((item, index) => (
+                <Grid item md={4} sm={12}>
+                  <Card sx={{ minWidth: '245px', backgroundColor: '#F7F9FF', margin: { xs: '16px 16px', md: '0px 16px' }, height: { md: '100%' } }} key={index}>
+                    <CardContent>
+                      <Box component={'div'} display={'flex'} flexDirection={'column'} alignItems={'center'} gap={'8px'}>
+                        <Box component={'div'} display={'flex'}>{item.icone}</Box>
+                        <Box component={'span'} display={'flex'} flexWrap={'nowrap'} fontWeight={'bold'} textTransform={'uppercase'}>{item.title}</Box>
+                        <Box component={'span'} display={'flex'} flexWrap={'nowrap'}>{item.informations}</Box>
+                      </Box>
+                    </CardContent>
+                  </Card>
+                </Grid>
+              ))}
             </Grid>
-            <Button
-              type="submit"
-              onClick={() => formik.handleSubmit()}
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, background: '#6813D2', borderRadius: '4px' }}
-            >
-              Enviar
-            </Button>
-          </Box>
-        </Container>
+            <Box component={'div'} display={'flex'} alignItems={'center'} flexDirection={'column'} border={'1px solid #CCCCCC'} borderRadius={4} m={'16px 16px'}>
+              <Typography variant="inherit" lineHeight={1.5} padding={'16px 16px'}>
+                Você ama desafios? Sonha em criar tecnologia que muda o mundo? Então dê o primeiro passo para uma carreira emocionante com o Curso de Engenharia da Computação!
+                Por que escolher Engenharia da Computação conosco?
+              </Typography>
+              {AccordionInformations.map((item, index) => (
+                <Accordion key={index + 1} sx={{ width: '100%' }}>
+                  <AccordionSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls={`panel${index + 1}-content`}
+                    id={`panel${index + 1}-header`}
+                  >
+                    {item.title}
+                  </AccordionSummary>
+                  <AccordionDetails>
+                    {item.informations}
+                  </AccordionDetails>
+                </Accordion>
+              ))}
+            </Box>
+          </Grid>
+          <Grid item md={4} sm={12} display={'flex'} alignItems={'center'}>
+            <Container>
+              <Typography variant="h4" display={'flex'} justifyContent={'center'}>Formulário de Inscrição</Typography>
+              <Box component={'div'} sx={{ mt: 3 }}>
+                <Grid container spacing={2}>
+                  <Grid item xs={12} sm={12} md={12}>
+                    <TextField
+                      autoComplete="given-name"
+                      name="nome"
+                      fullWidth
+                      id="nome"
+                      label="Nome *"
+                      autoFocus
+                      value={formik.values.nome}
+                      onChange={formik.handleChange}
+                      error={Boolean(formik.errors.nome)}
+                    />
+                  </Grid>
+                  <Grid item xs={12} sm={12} md={12}>
+                    <TextField
+                      fullWidth
+                      id="sobrenome"
+                      label="Sobrenome *"
+                      name="sobrenome"
+                      autoComplete="family-name"
+                      value={formik.values.sobrenome}
+                      onChange={formik.handleChange}
+                      error={Boolean(formik.errors.sobrenome)}
+                    />
+                  </Grid>
+                  <Grid item xs={12} sm={12} md={12}>
+                    <TextField
+                      type="email"
+                      fullWidth
+                      id="email"
+                      label="Email *"
+                      name="email"
+                      autoComplete="email"
+                      value={formik.values.email}
+                      onChange={formik.handleChange}
+                      error={Boolean(formik.errors.email)}
+                    />
+                  </Grid>
+                  <Grid item xs={12} sm={12} md={12}>
+                    <TextField
+                      fullWidth
+                      name="telefone_celular"
+                      label="Telefone Celular *"
+                      id="telefone_celular"
+                      autoComplete="cellphone"
+                      value={formik.values.telefone_celular}
+                      onChange={formik.handleChange}
+                      InputProps={{
+                        inputComponent: ReactInputMask as any,
+                        inputProps: {
+                          mask: "(99)99999-9999",
+                          maskChar: " ",
+                        },
+                      }}
+                      error={Boolean(formik.errors.telefone_celular)}
+                    />
+                  </Grid>
+                  <Grid item xs={12} sm={12} md={12}>
+                    <FormControl fullWidth>
+                      <InputLabel id="-label">Horário *</InputLabel>
+                      <Select
+                        fullWidth
+                        labelId="-label"
+                        id="horario"
+                        name="horario"
+                        label="Horário *"
+                        value={formik.values.horario}
+                        onChange={formik.handleChange}
+                        error={Boolean(formik.errors.horario)}
+                      >
+                        <MenuItem value={'14as16'}>Das 14h às 18h</MenuItem>
+                        <MenuItem value={'19as23'}>Das 19h às 23h</MenuItem>
+                      </Select>
+                    </FormControl>
+                  </Grid>
+                </Grid>
+                <Button
+                  type="submit"
+                  onClick={() => formik.handleSubmit()}
+                  fullWidth
+                  variant="contained"
+                  sx={{ mt: 3, background: '#6813D2', borderRadius: '4px' }}
+                >
+                  Enviar
+                </Button>
+              </Box>
+            </Container>
+          </Grid>
+        </Grid>
       </Box>
     </Box>
   )
